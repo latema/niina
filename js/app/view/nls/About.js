27c27d31edb05c6a475c605230b2b1e8ff0cfa2e
Lauri Maasola <lauri.maasola@gmail.com>
2015-06-01 20:11:17 +0200

    remove pt2 of about text

--- js/app/view/nls/About.js
+++ js/app/view/nls/About.js
@@ -1,7 +1,6 @@
 define({
     "root": {
         "about_pt1": "My works study my own relationship with media imagery. Sometimes it is hard to accept and understand the daily information. I have noticed that I often mix the documentary information with the fictional narrative.",
-        "about_pt2": "When I document my own media memories I want to show how we all live in this same self-repetitive media environment.",
         "cv_pt1": "You can download my CV",
         "cv_pt2": "here.",
         "cv_path": "\"./img/Niina_Huovinen_E.pdf\""
--- js/app/view/nls/fi/About.js
+++ js/app/view/nls/fi/About.js
@@ -1,6 +1,5 @@
 define({
     "about_pt1": "Teoksissani kuvaan omaa suhdettani mediakuvastoon. Uutiskuvaston vastaanottaminen ja sen ymmärtäminen tuntuu joskus vaikealta. Huomaan, kuinka dokumentaarinen informaatio sekoittuu päässäni fiktiiviseen kerrontaan.",
-    "about_pt2": "Dokumentoimalla uutiskuvaston tuottamia muistoja, haluan tuoda esille, kuinka me kaikki elämme tässä samassa, itseään jatkuvasti toistavassa mediaympäristössä.",
     "cv_pt1": "Voit ladata CV:ni",
     "cv_pt2": "tästä.",
     "cv_path": "\"./img/Niina_Huovinen_F.pdf\""
